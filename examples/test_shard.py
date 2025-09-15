296cfe417594f395c7b8961dcc4f5012cc218b00
Leo Liu <leoszliu@gmail.com>
2025-09-15 13:10:37 -0700

    Add test files for 1K and 1M documents

---
+++ examples/test_shard.py
@@ -0,0 +1,40 @@
+import time
+import numpy as np
+import pickle
+from graphgrove.sgtree import NNS_L2 as SGTree_NNS_L2
+from graphgrove.covertree import NNS_L2 as CoverTree_NNS_L2
+
+gt = time.time
+
+
+cores = 4
+
+print('======== Building Dataset ==========')
+
+d = np.load('/drive_sdc/msmarco-mpnet/shards/mpnet-msmarco-train-embeddings-default-768-shard-4.npz')
+P = d['passage_embeddings'][:1000]
+Q = d['query_embeddings'][:10]
+
+N = P.shape[0]
+D = P.shape[1]
+P = np.require(P, requirements=['A','C','O','W'])
+
+print('======== SG Tree ==========')
+t = gt()
+ct = SGTree_NNS_L2.from_matrix(P, use_multi_core=cores, new_base=1.3)
+b_t = gt() - t
+#ct.display()
+print("Building time:", b_t, "seconds")
+
+ct.dump_tree('MSMARCO_1000x10_base_1.3.json')
+
+print('Test k-Nearest Neighbours - Exact (k=1): ')
+t = gt()
+idx1, d1 = ct.NearestNeighbour(Q, use_multi_core=cores, filename='MSMARCO_1000x10_base_1.3.trace')
+b_t = gt() - t
+print("Query time - Exact:", b_t, "seconds")
+
+
+# G = pickle.load(open('/drive_sdc/msmarco-mpnet/mpnet-msmarco-train-embeddings-default-768-QP_gold.pkl','rb'))
+
+
